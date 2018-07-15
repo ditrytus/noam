185141f84abb52ad941b4ad1b4456341d635d6ab
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2018-07-15 10:55:25 +0200

    Removed auto generated files.

--- src/library.cpp
+++
@@ -1,5 +0,0 @@
-#include "library.h"
-
-int one() {
-    return 1;
-}
--- test/library_test.cpp
+++
@@ -1,6 +0,0 @@
-#include <include/gtest/gtest.h>
-#include "library.h"
-
-TEST(LibraryTest, ReturnsOne) {
-    EXPECT_EQ(one(), 1) << "one() should return integer 1.";
-}
