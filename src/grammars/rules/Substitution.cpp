583cbd2736ac2e5a6241727093f644728c753bdb
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2018-08-11 23:14:21 +0200

    Revert "Removed reference to c++20 standard."
    This reverts commit 8d3eec7

--- src/grammars/rules/Substitution.cpp
+++ src/grammars/rules/Substitution.cpp
@@ -82,15 +82,6 @@ auto& symbolsA = a.getSymbols();
 bool noam::operator==(const Substitution &a, const Substitution &b) {
     auto aSymbols = a.getSymbols();
     auto bSymbols = b.getSymbols();
-    auto aCursor = aSymbols.begin();
-    auto aLast = aSymbols.end();
-    auto bCursor = bSymbols.begin();
-    auto bLast = bSymbols.end();
-
-    SharedPtrObjectsEqualityComparer<Symbol> comp{};
-
-    while (aCursor != aLast && bCursor != bLast && comp(*aCursor, *bCursor)) {
-        ++aCursor, ++bCursor;
-    }
-    return aCursor == aLast && bCursor == bLast;
+    auto miss = mismatch(aSymbols.begin(), aSymbols.end(), bSymbols.begin(), bSymbols.end(), SharedPtrObjectsEqualityComparer<Symbol>{});
+    return miss.first == aSymbols.end() && miss.second == bSymbols.end();
 }
